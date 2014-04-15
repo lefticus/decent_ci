d356c7c8c9003d77ec0fededcc88091e58fff870
Jason Turner <jason@emptycrate.com>
2014-04-15 15:25:19 -0600

    Enable timeout / kill of builds if they go too long.
     - The time is currently hardcoded, that will need to be addressed
       at some point

--- ci.rb
+++ ci.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8 
+
 require 'octokit'
 require 'json'
 require 'open3'
@@ -423,13 +425,68 @@ class PotentialBuild
     return @pull_id
   end
 
+  # originally from https://gist.github.com/lpar/1032297
+  # runs a specified shell command in a separate thread.
+  # If it exceeds the given timeout in seconds, kills it.
+  # Returns any output produced by the command (stdout or stderr) as a String.
+  # Uses Kernel.select to wait up to the tick length (in seconds) between 
+  # checks on the command's status
+  #
+  # If you've got a cleaner way of doing this, I'd be interested to see it.
+  # If you think you can do it with Ruby's Timeout module, think again.
+  def run_with_timeout(command, timeout=60*60*4, tick=2)
+    out = ""
+    err = ""
+    begin
+      # Start task in another thread, which spawns a process
+      stdin, stdout, stderr, thread = Open3.popen3(command)
+      # Get the pid of the spawned process
+      pid = thread[:pid]
+      start = Time.now
+
+      while (Time.now - start) < timeout and thread.alive?
+        # Wait up to `tick` seconds for output/error data
+        rs, = Kernel.select([stdout, stderr], nil, nil, tick)
+        # Try to read the data
+        begin
+          rs.each { |r|
+            if r == stdout
+              out << stdout.read_nonblock(4096)
+            elsif r == stderr 
+              err << stderr.read_nonblock(4096)
+            end
+          }
+
+        rescue IO::WaitReadable
+          # A read would block, so loop around for another select
+        rescue EOFError
+          # Command has completed, not really an error...
+          break
+        end
+      end
+      # Give Ruby time to clean up the other thread
+      sleep 1
+
+      if thread.alive?
+        # We need to kill the process, because killing the thread leaves
+        # the process alive but detached, annoyingly enough.
+        Process.kill("TERM", pid)
+      end
+    ensure
+      stdin.close if stdin
+      stdout.close if stdout
+      stderr.close if stderr
+    end
+    return out.force_encoding("UTF-8"), err.force_encoding("UTF-8"), thread.value
+  end
+
   def run_script(commands)
     allout = ""
     allerr = "" 
     allresult = 0
 
     commands.each { |cmd|
-      stdout, stderr, result = Open3.capture3(cmd)
+      stdout, stderr, result = run_with_timeout(cmd)
 
       stdout.split("\n").each { |l| 
         @logger.debug("cmd: #{cmd}: stdout: #{l}")
@@ -857,6 +914,11 @@ class PotentialBuild
   end
 
   def next_build
+    @created_dirs = []
+    @package_location = nil
+    @test_results = nil
+    @build_results = SortedSet.new()
+    @package_results = SortedSet.new()
     @dateprefix = nil
   end
 
