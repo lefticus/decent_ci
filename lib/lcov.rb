919b04a7619b4a7b6c1245bb5d782c2ac531e55f
Jason Turner <jason@emptycrate.com>
2015-01-12 14:45:39 -0700

    Remove all generated files from coverage reporting

--- lib/lcov.rb
+++ lib/lcov.rb
@@ -7,7 +7,10 @@ module Lcov
     out, err, result = run_script(
       ["cd #{build_dir} && lcov #{lcov_flags}"])
 
-    genhtml_flags = "./lcov.output -o lcov-html --demangle-cpp --function-coverage --rc genhtml_hi_limit=#{compiler[:coverage_pass_limit]}  --rc genhtml_med_limit=#{compiler[:coverage_warn_limit]}"
+    out, err, result = run_script(
+      ["cd #{build_dir} && lcov -r ./lcov.output `pwd`/\\* -o ./lcov.output.filtered"])
+
+    genhtml_flags = "./lcov.output.filtered -o lcov-html --demangle-cpp --function-coverage --rc genhtml_hi_limit=#{compiler[:coverage_pass_limit]}  --rc genhtml_med_limit=#{compiler[:coverage_warn_limit]}"
     out, err, result = run_script(
       ["cd #{build_dir} && genhtml #{genhtml_flags}"])
 
