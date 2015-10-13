9898cc464849cb8923eac47b9031d670a2f6b8b3
Jason Turner <jason@emptycrate.com>
2015-10-13 17:40:11 -0600

    Add forgotten utility.rb

---
+++ lib/utility.rb
@@ -0,0 +1,21 @@
+def try_hard_to_remove_dir d
+
+  5.times {
+    begin
+      FileUtils.rm_rf(d)
+      $logger.debug("Succeeded in cleaning up #{d}")
+      return
+    rescue => e
+      $logger.error("Error cleaning up directory #{e}, sleeping and probably trying again")
+      sleep(1)
+    end
+  }
+
+  $logger.error("Failed in cleaning up directory #{e}")
+
+end
+
+
+def add_global_created_dir d
+  $created_dirs << d
+end
