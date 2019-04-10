5487c4838e1dd3bc9fd9e0b080d34962cbb9b6f9
Edwin Lee <leeed2001@gmail.com>
2019-04-10 13:10:05 -0500

    Add badges to readme

--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -3,7 +3,10 @@
 # we use simplecov because that makes coverage results available locally
 require 'simplecov'
 SimpleCov.start
-
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
+                                                                    SimpleCov::Formatter::HTMLFormatter,
+                                                                    SimpleCov::Formatter::CSVFormatter,
+                                                                ])
 # but for CI we use coveralls.io
 require 'coveralls'
 Coveralls.wear!('rails')
