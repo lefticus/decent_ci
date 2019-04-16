9d03e95d6d32fe9d67794b72bffab1ddee4bb58e
Edwin Lee <leeed2001@gmail.com>
2019-04-16 09:47:07 -0500

    Fixup code climate concern

--- spec/cmake_spec.rb
+++ spec/cmake_spec.rb
@@ -2,29 +2,47 @@ require 'rspec'
 require_relative '../lib/cmake'
 require_relative '../lib/configuration'
 
+class DummyRegressionBuild
+  attr_reader :this_build_dir
+  attr_reader :commit_sha
+  def initialize(build_dir, sha)
+    @this_build_dir = build_dir
+    @commit_sha = sha
+  end
+end
+
 describe 'CMake Testing' do
   include CMake
   include Configuration
 
   context 'when calling cmake_build' do
-    it 'should try to build', :focus do
+    it 'should try to build a base release package', :focus do
       allow_any_instance_of(Runners).to receive(:run_scripts).and_return(['stdoutmsg', 'stderrmsg', 0])
       allow_any_instance_of(Octokit::Client).to receive(:content).and_return([NamedDummy.new('.decent_ci.yaml')])
-
       @client = Octokit::Client.new(:access_token => 'abc')
       @config = load_configuration('spec/resources', 'abc', false)
       compiler = @config.compilers.first
-
       src_dir = Dir.mktmpdir
       build_dir = File.join(src_dir, 'build')
-
-      regression_dir = Dir.mktmpdir
+      regression_dir = nil
       regression_baseline = nil
-
       @build_results = SortedSet.new
-
       args = CMakeBuildArgs.new('Debug', 'thisDeviceIDHere', true, true)
-
+      response = cmake_build(compiler,src_dir, build_dir, regression_dir, regression_baseline, args)
+      expect(response).to be_truthy
+    end
+    it 'should try to build a release with regressions', :focus do
+      allow_any_instance_of(Runners).to receive(:run_scripts).and_return(['stdoutmsg', 'stderrmsg', 0])
+      allow_any_instance_of(Octokit::Client).to receive(:content).and_return([NamedDummy.new('.decent_ci.yaml')])
+      @client = Octokit::Client.new(:access_token => 'abc')
+      @config = load_configuration('spec/resources', 'abc', false)
+      compiler = @config.compilers.first
+      src_dir = Dir.mktmpdir
+      build_dir = File.join(src_dir, 'build')
+      regression_dir = Dir.mktmpdir
+      regression_baseline = DummyRegressionBuild.new('/dir/', 'abcd')
+      @build_results = SortedSet.new
+      args = CMakeBuildArgs.new('Debug', 'thisDeviceIDHere', true, true)
       response = cmake_build(compiler,src_dir, build_dir, regression_dir, regression_baseline, args)
       expect(response).to be_truthy
     end
